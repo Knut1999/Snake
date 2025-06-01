a17ccd7f17fa9c7e36db87f8b6e465e5cc6c0f61
Knut Onsøyen <kgo@ingenia.no>
2025-06-01 21:58:57 +0200

    oppdatert innhold

--- snake.js
+++ snake.js
@@ -72,7 +72,7 @@ function update() {
   if (snake_hodeX < 0){
     snake_hodeX = 30 * squere_size;
   }
-  else if (snake_hodeX > board.length * squere_size){
+  else if (snake_hodeX > rad * squere_size){
     snake_hodeX = 0;
   }
 }
