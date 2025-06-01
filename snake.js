ed3f104f8c1ca5ab41969d4ff6cc6748b450c4b4
Knut Onsøyen <kgo@ingenia.no>
2025-06-01 13:00:33 +0200

    oppdatert innhold

--- snake.js
+++ snake.js
@@ -2,13 +2,13 @@ var squere_size = 20;
 var rad = 30;
 var kolonne = 30;
 
-var snake_hodeX = 10;
-var snake_hodeY = 10;
+var snake_hodeX = 10 * squere_size;
+var snake_hodeY = 10 * squere_size;
 
 var snake_kropp = [];
 
-var mat_X = 15;
-var mat_Y = 15;
+var mat_X = 15 * squere_size;
+var mat_Y = 15 * squere_size;
 
 window.onload = function() {
   board = document.getElementById("board");
@@ -17,5 +17,5 @@ window.onload = function() {
   context = board.getContext("2s"); //for å tegne på canvas
 
   context.fillStyle = "lime";
-  context.fillRect(snake_hodeX, snake_hodeY, squere_size, squere_size)
+  context.fillRect(mat_X, mat_Y, squere_size, squere_size)
 }
